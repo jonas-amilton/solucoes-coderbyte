ec2adef99e3f2f687ffd518be2e804eaecb14c87
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2022-07-27 15:56:44 -0300

    Update function FirstReverse.js

--- function FirstReverse.js
+++ function FirstReverse.js
@@ -1,6 +1,18 @@
+/*
+First Reverse
+HIDE QUESTION
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+Modifique a função FirstReverse(str) para que pegue o parâmetro str sendo passado e devolva a sequência em ordem invertida.
+Por exemplo: se a sequência de entrada for "Hello World and Coders" então seu programa deve retornar a string sredoC dna dlroW olleH.
+*/
+
+//solution
+//solução
+
 function FirstReverse(string) {
     return string.split('').reverse().join('');
   }
      
-  // keep this function call here 
   FirstReverse(readline());
