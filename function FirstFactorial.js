8591be7edbfd4ae9e60206c187cf134a3e6513f9
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2022-07-27 15:52:25 -0300

    Update function FirstFactorial.js

--- function FirstFactorial.js
+++ function FirstFactorial.js
@@ -1,3 +1,18 @@
+/*
+First Factorial
+HIDE QUESTION
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
+For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+Modifique a função FirstFactorial(num) para que pegue o parâmetro numérico que está sendo passado e devolva o fatorial dele.
+Por exemplo: se num = 4, então seu programa deve retornar (4 * 3 * 2 * 1) = 24.
+Para os casos de teste, a faixa será entre 1 e 18 e a entrada será sempre um inteiro.
+
+*/
+
+//solution
+//solução
 function FirstFactorial(num) { 
 
     if (num === 0) {
